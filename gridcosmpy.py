03f5c3948c1e904fded2cf87e957b97623f46f9d
rggassner <rafael.gassner@gmail.com>
2022-07-04 20:59:27 -0300

    Update gridcosmpy.py

--- gridcosmpy.py
+++ gridcosmpy.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import multiprocessing
 import requests
 import ffmpeg
 import math
@@ -10,9 +11,14 @@ from PIL import Image
 
 zoom_ratio=.98
 IMAGES_FOLDER="images/"
+OIMAGES_FOLDER="imagesi/"
 SCALED_FOLDER="scaled/"
 TRIMS_FOLDER="trims/"
-im_width=450
+im_width=3600
+NPROCS=5
+INPUT_FORMAT='png'
+OUTPUT_FORMAT='jpg'
+ENLARGE=False
 
 class TLSAdapter(requests.adapters.HTTPAdapter):
     def init_poolmanager(self, *args, **kwargs):
@@ -28,41 +34,58 @@ def get_current(session=""):
 
 def sync_images(current,session=""):
     for level in range(current):
-        if not exists(IMAGES_FOLDER+str(level)+'.jpg'):
+        if not exists(OIMAGES_FOLDER+str(level)+'.jpg'):
             img_data = res = session.get('https://www.sito.org/synergy/gridcosm/pieces/'+str(level).zfill(3)+'-f.jpg').content
-            with open(IMAGES_FOLDER+str(level)+'.jpg', 'wb') as handler:
+            with open(OIMAGES_FOLDER+str(level)+'.jpg', 'wb') as handler:
                 handler.write(img_data)
 
-def load_image(filename):
-    filename=IMAGES_FOLDER+str(filename)+'.jpg'
+def load_image(filename,folder=IMAGES_FOLDER,fformat=INPUT_FORMAT):
+    filename=folder+str(filename)+'.'+fformat
     return Image.open(filename)
 
 def enlarge(image):
-    waifu2x = Waifu2x(gpuid=-1, scale=2, noise=3)
+    waifu2x = Waifu2x(gpuid=0, scale=8, noise=3)
     return waifu2x.process(image)
 
 def gen_frames_out(current):
-    pointer=0
-    frame_count=0
-    while(pointer<current-1):
-        width = im_width * 3
-        czoom_ratio=zoom_ratio
-        while (width*zoom_ratio) >= im_width:
-            working_frame = load_image(pointer+1).resize((im_width*3,im_width*3), Image.ANTIALIAS)
-            working_frame.paste(load_image(pointer),(im_width,im_width))
+    pool = multiprocessing.Pool(NPROCS)
+    result = pool.map(gen_frame_out, range(0,current-1))
+
+def gen_frame_out(current):
+    print('Starting {}'.format(current))
+    frame_count=1000*current
+    width = im_width * 3
+    czoom_ratio=zoom_ratio
+    while (width*zoom_ratio) >= im_width:
+        #If the frame does not exist
+        if not exists(SCALED_FOLDER+str(frame_count).zfill(12)+".jpg"):
+            working_frame = load_image(current+1).resize((im_width*3,im_width*3), Image.ANTIALIAS)
+            working_frame.paste(load_image(current),(im_width,im_width))
+            if (current-1) >= 0:
+                previous=load_image(current-1,folder=IMAGES_FOLDER).resize((int(math.floor(im_width/3)),int(math.floor(im_width/3))), Image.ANTIALIAS)
+                working_frame.paste(previous,(im_width+int(math.floor(im_width/3)),im_width+int(math.floor(im_width/3))))
+                if (current-2) >= 0:
+                    pprevious=load_image(current-2,folder=IMAGES_FOLDER).resize((int(math.floor(im_width/9)),int(math.floor(im_width/9))), Image.ANTIALIAS)
+                    working_frame.paste(pprevious,(im_width+int(math.floor(im_width/3+im_width/9)),im_width+int(math.floor(im_width/3+im_width/9)) ))
             working_frame = working_frame.resize((int(math.floor(im_width*3*czoom_ratio)),int(math.floor(im_width*3*czoom_ratio))), Image.ANTIALIAS)
             nwidth, width = working_frame.size
             working_frame=working_frame.crop((math.floor((nwidth/2)-(im_width/2)),math.floor((nwidth/2)-(im_width/2)),math.floor((nwidth/2)+(im_width/2)),math.floor((nwidth/2)+(im_width/2))))
-            if not exists(SCALED_FOLDER+str(frame_count).zfill(8)+".jpg"):
-                enlarge(working_frame).save(SCALED_FOLDER+str(frame_count).zfill(8)+".jpg")
-            frame_count=frame_count+1
-            czoom_ratio=czoom_ratio*zoom_ratio
-        pointer=pointer+1
+            if ENLARGE:
+                enlarge(working_frame).save(SCALED_FOLDER+str(frame_count).zfill(12)+".jpg")
+            else:
+                working_frame.save(SCALED_FOLDER+str(frame_count).zfill(12)+".jpg")
+        #Quite rare condition, used to avoid generating a image smaller than the video size after a resumed run
+        else:
+            nwidth, width = load_image(str(frame_count).zfill(12),folder=SCALED_FOLDER,fformat=OUTPUT_FORMAT).size
+        frame_count=frame_count+1
+        czoom_ratio=czoom_ratio*zoom_ratio
+    print('   Finished {}'.format(current))
+    return True
 
 def gen_video():
     output = ( ffmpeg
     .input(SCALED_FOLDER+'*.jpg', pattern_type='glob', framerate=25)
-    .filter('crop',im_width*2,int(math.floor((im_width*2)/1.77777)))
+    .filter('crop',im_width,int(math.floor(im_width/1.77777)))
     .output('gridcosmpy.mp4')
     .run())
 
@@ -72,3 +95,4 @@ current=get_current(session=session)
 sync_images(current,session=session)
 gen_frames_out(current)
 gen_video()
+
