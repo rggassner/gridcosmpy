04b25fdbfccfa9e363d2e558ba2f609dea4f20c4
rggassner <rafael.gassner@gmail.com>
2023-04-06 21:02:22 -0300

    Update autogrid.py

--- autogrid/autogrid.py
+++ autogrid/autogrid.py
@@ -3,17 +3,21 @@ from diffusers import StableDiffusionInpaintPipeline
 import torch,argparse
 from PIL import Image
 
-steps=50
 noptions=25
-allow_nsfw=False
+allow_nsfw=True
 
 def dummy(images, **kwargs):
     return images, False
 
 argParser = argparse.ArgumentParser()
 argParser.add_argument("-t", "--text",required=True,help="Text that will be used to inpaint the image.")
+argParser.add_argument("-n", "--negative",required=True,help="Negative prompt.")
+argParser.add_argument("-s", "--steps",required=True,help="Number of steps.")
 args = argParser.parse_args()
 prompt=args.text
+negative_prompt=args.negative
+steps=int(args.steps)
+negative_prompt.strip()
 prompt=prompt.strip()
 pipe = StableDiffusionInpaintPipeline.from_pretrained( "runwayml/stable-diffusion-inpainting",torch_dtype=torch.float16).to('cuda')
 if allow_nsfw:
@@ -23,5 +27,5 @@ for iteration in range(1,noptions):
     torch.cuda.empty_cache()
     init_image_arr = Image.open('img/input.png')
     mask_image_arr = Image.open('img/mask.png')
-    outpainted_image = pipe(prompt=prompt, image=init_image_arr, mask_image=mask_image_arr,num_inference_steps=steps).images[0]
+    outpainted_image = pipe(prompt=prompt, negative_prompt=negative_prompt,image=init_image_arr, mask_image=mask_image_arr,num_inference_steps=steps).images[0]
     outpainted_image.save('tmp/'+str(iteration)+'.png')
