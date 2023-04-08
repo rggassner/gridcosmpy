0854c38a1501dad87224bec1368a4fe3fe9e12cb
rggassner <rafael.gassner@gmail.com>
2023-04-08 00:19:02 -0300

    Add files via upload

---
+++ sync_wget.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+current=`wget -O - https://www.sito.org/cgi-bin/gridcosm/gridcosm 2> /dev/null | grep "<title>SITO - Gridcosm level .*</title>" | sed "s/[^0-9]//g"`
+download_dir="gridcosmpy/original"
+for image in `seq -f "%03g" 0 $current`
+do
+        if [[ ! -f "$download_dir/$image-f.jpg" ]]
+        then
+                wget https://www.sito.org/synergy/gridcosm/pieces/$image-f.jpg --directory-prefix=$download_dir
+        fi
+done
