2792f132369df47a86ec7f584d7804f6bc7b6ede
rggassner <rafael.gassner@gmail.com>
2023-04-08 00:17:39 -0300

    Update render_str2imgzoom.sh

--- render_str2imgzoom.sh
+++ render_str2imgzoom.sh
@@ -3,6 +3,9 @@ rm gridcosmpy.mp4
 rm gridcosmpy_reversed.mp4
 rm input/*
 rm input_upscaled/*
+rm output_frames/*
+rm reversed/tmp/*
+rm reversed/*.jpg
 cp str2imgzoom/out/*[0-9].png input/
-waifu2x-ncnn-vulkan-20220728-ubuntu/waifu2x-ncnn-vulkan -f png -i input/ -o input_upscaled/ -s 8 -n 3 -g 0
+waifu2x-ncnn-vulkan-20220728-ubuntu/waifu2x-ncnn-vulkan -f png -i input/ -o input_upscaled/ -s 8 -n 3 -g 0 
 ./gridcosmpy.py -r 4
