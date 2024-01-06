79835a576768ad51d6600c60da6865a7b95ff1d7
rggassner <rafael.gassner@gmail.com>
2024-01-06 14:39:26 -0300

    Update sync_wget.sh

--- sync_wget.sh
+++ sync_wget.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 current=`wget -O - https://www.sito.org/cgi-bin/gridcosm/gridcosm 2> /dev/null | grep "<title>SITO - Gridcosm level .*</title>" | sed "s/[^0-9]//g"`
-download_dir="gridcosmpy/original"
+download_dir="original"
 for image in `seq -f "%03g" 0 $current`
 do
         if [[ ! -f "$download_dir/$image-f.jpg" ]]
