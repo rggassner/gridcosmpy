a49edbb054769787d5ef8fcdd3d935eccf29dd55
rggassner <rafael.gassner@gmail.com>
2024-01-22 18:19:54 -0300

    Update gridcosmpy.py

--- gridcosmpy.py
+++ gridcosmpy.py
@@ -15,16 +15,21 @@ NPROCS=5
 INPUT_FORMAT='png'
 
 def load_image(filename,folder=IMAGES_FOLDER,fformat=INPUT_FORMAT):
-    filename=folder+str(filename)+'.'+fformat
+    filename=folder+str(filename).zfill(3)+'-f.'+fformat
     return Image.open(filename)
 
 def gen_frames_out(current):
     pool = multiprocessing.Pool(NPROCS)
-    result = pool.map(gen_frame_out, range(0,current-1))
+    result = pool.map(gen_frame_out, range(0,current-2))
 
 def get_image_size():
-    im = Image.open(IMAGES_FOLDER+'/0.png')
-    return im.size[0]
+    png_files = [f for f in os.listdir(IMAGES_FOLDER) if f.lower().endswith('.png')]
+    if png_files:
+        first_png_path = os.path.join(IMAGES_FOLDER, png_files[0])
+        im = Image.open(first_png_path)
+        return im.size[0]
+    else:
+        return None  
 
 def gen_frame_out(current):
     print('Starting {}'.format(current))
@@ -33,7 +38,7 @@ def gen_frame_out(current):
     czoom_ratio=zoom_ratio
     while (width*zoom_ratio) >= im_width:
         #If the frame does not exist
-        if not exists(SCALED_FOLDER+str(frame_count).zfill(12)+".jpg"):
+        if not exists(SCALED_FOLDER+str(frame_count).zfill(12)+".png"):
             #Create a large fame using the current one
             working_frame = load_image(current+1).resize((im_width*imrate,im_width*imrate), Image.LANCZOS)
             #Paste previous frame
@@ -60,7 +65,7 @@ def gen_frame_out(current):
             nwidth, width = working_frame.size
             #Crop the center of the image to get the desired dimension
             working_frame=working_frame.crop((math.floor((nwidth/2)-(im_width/2)),math.floor((nwidth/2)-(im_width/2)),math.floor((nwidth/2)+(im_width/2)),math.floor((nwidth/2)+(im_width/2))))
-            working_frame.save(SCALED_FOLDER+str(frame_count).zfill(12)+".jpg")
+            working_frame.save(SCALED_FOLDER+str(frame_count).zfill(12)+".png")
         #Quite rare condition, used to avoid generating a image smaller than the video size after a resumed run
         else:
             width = int(math.floor(im_width*imrate*czoom_ratio))
@@ -71,32 +76,32 @@ def gen_frame_out(current):
 
 def gen_video():
     output = ( ffmpeg
-    .input(SCALED_FOLDER+'*.jpg', pattern_type='glob', framerate=25)
+    .input(SCALED_FOLDER+'*.png', pattern_type='glob', framerate=25)
     .filter('crop',im_width,int(math.floor(im_width/1.77777)))
     .output('gridcosmpy.mp4')
     .run())
     
 def gen_video_reversed():
     output = ( ffmpeg
-    .input('reversed/tmp/'+'*.jpg', pattern_type='glob', framerate=25)
+    .input('reversed/tmp/'+'*.png', pattern_type='glob', framerate=25)
     .output('gridcosmpy_reversed.mp4')
     .run(quiet=True))
 
 def split_images():
     output= (ffmpeg
     .input('gridcosmpy.mp4')
-    .output('reversed/%d.jpg', start_number=0)
+    .output('reversed/%d.png', start_number=0)
     .overwrite_output()
     .run(quiet=True))
 
 def rename_reversed():
     largest=0
     for filename in os.listdir('reversed'):
-        if filename.endswith('.jpg'): 
+        if filename.endswith('.png'): 
             if int(Path(filename).stem) > largest:
                 largest=int(Path(filename).stem)
     for num in range(0,int(largest+1)):
-        os.rename('reversed/'+str(num)+'.jpg','reversed/tmp/'+str(largest-num).zfill(12)+'.jpg')
+        os.rename('reversed/'+str(num)+'.png','reversed/tmp/'+str(largest-num).zfill(12)+'.png')
 
 def get_current():
     count=0
@@ -112,8 +117,7 @@ imrate=args.rate
 im_width=get_image_size()
 current=get_current()
 gen_frames_out(current)
-gen_video()
+#gen_video()
 split_images()
 rename_reversed()
 gen_video_reversed()
-#assure output croped size is 4k, no matter what input size it is, or maybe even select output resolution
