563171e4530c97e1b64fe39c3caf77fefb8e18cc
rggassner <rafael.gassner@gmail.com>
2025-06-01 21:22:45 -0300

    Update 4-genOptions.py

--- str2imgzoom/4-genOptions.py
+++ str2imgzoom/4-genOptions.py
@@ -1,34 +1,45 @@
-#!/bin/python3
+#!venv/bin/python3
 from diffusers import StableDiffusionInpaintPipeline
-import torch,sys
+import torch, sys
 from PIL import Image
 
-steps=50
-noptions=27
+steps = 50
+noptions = 27
 
 def dummy(images, **kwargs):
-    return images, False
+    return images, [False] * len(images)  # Fix: make it iterable
 
-def gen_out(image,iteration):
+def gen_out(image, iteration):
     old_im = image.resize((128, 128))
-    old_size = old_im.size
-    new_size = (512, 512)
-    new_im = Image.new("RGB", new_size) 
-    box = tuple((n - o) // 2 for n, o in zip(new_size, old_size))
+    new_im = Image.new("RGB", (512, 512))
+    box = tuple((n - o) // 2 for n, o in zip((512, 512), old_im.size))
     new_im.paste(old_im, box)
-    new_im.save('tmp/scaled/'+str(iteration)+'-scaled.png')
+    new_im.save(f'tmp/scaled/{iteration}-scaled.png')
 
-prompt=sys.argv[1]
-prompt=prompt.strip()
-count=sys.argv[2]
+prompt = sys.argv[1].strip()
+count = sys.argv[2]
 
-pipe = StableDiffusionInpaintPipeline.from_pretrained( "runwayml/stable-diffusion-inpainting",torch_dtype=torch.float16).to('cuda')
+pipe = StableDiffusionInpaintPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-2-inpainting",
+    torch_dtype=torch.float16
+).to("cuda")
+
 pipe.safety_checker = dummy
+
 print(prompt)
-for iteration in range(1,noptions):
+
+for iteration in range(1, noptions):
     torch.cuda.empty_cache()
-    init_image_arr = Image.open('out/'+str(int(count)-1)+'-scaled.png')
+    init_image_arr = Image.open(f'out/{int(count)-1}-scaled.png')
     mask_image_arr = Image.open('img/mask.png')
-    outpainted_image = pipe(prompt=prompt, image=init_image_arr, mask_image=mask_image_arr,num_inference_steps=steps).images[0]
-    outpainted_image.save('tmp/img/'+str(iteration)+'.png')
-    gen_out(outpainted_image,iteration)
+    
+    outpainted_image = pipe(
+        prompt=prompt,
+        image=init_image_arr,
+        mask_image=mask_image_arr,
+        num_inference_steps=steps
+    ).images[0]
+    
+    outpainted_image.save(f'tmp/img/{iteration}.png')
+    gen_out(outpainted_image, iteration)
+
